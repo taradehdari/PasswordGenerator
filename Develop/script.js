78f329d8b7b35533e84680b3c69c6e2d55e18908
Tara Dehdari <tara.dehdari@gmail.com>
2020-08-01 21:33:12 -0700

    changed everything

--- Develop/script.js
+++ Develop/script.js
@@ -1,37 +1,58 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+const characterAmountRange = document.getElementById ('characterAmountRange')
+const characterAmountNumber = document.getElementById ('characterAmountNumber')
+const includeUppercaseElement = document.getElementById('includeUppercase')
+const includeNumbersElement = document.getElementById ('includeNumbers')
+const includeSymbolsElement = document.getElementById ('includeSymbols')
+const form = document.getElementById ('passwordGeneratorForm')
+const passwordDisplay = document.getElementById('passwordDisplay')
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+const UPPERCASE_CHAR_CODES = arrayFromLowToHigh(65, 90)
+const LOWERCASE_CHAR_CODES = arrayFromLowToHigh(97, 122)
+const NUMBER_CHAR_CODES = arrayFromLowToHigh(48, 57)
+const SYMBOL_CHAR_CODES = arrayFromLowToHigh(33, 47).concat(      arrayFromLowToHigh(58, 64)
+).concat (
+  arrayFromLowToHigh(91,96)
+).concat(
+  arrayFromLowToHigh(123, 126)
+)
 
-  passwordText.value = password;
-// generate random password 
-  function generate (){
+characterAmountNumber.addEventListener('input', syncCharacterAmount)
+characterAmountRange.addEventListener('input', syncCharacterAmount)
 
-    // password length/complexity
-    let complexity = document.getElementById("slider") 
-    
-    //possible password values
-    let values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+";
+form.addEventListener ('submit', e => {
+  e.preventDefault()
+  const characterAmount = characterAmountNumber.value
+  const includeUppercase = includeUppercaseElement.checked
+  const includeNumbers = includeNumbersElement.checked
+  const includeSymbols = includeSymbolsElement.checked
+  const password = generatePassword(characterAmount, includeUppercase, includeNumbers, includeSymbols)
+  passwordDisplay.interText = password
+})
 
-    let password = "";
-
-    // create for loop to choose password characters
-    for(var i=0; i <= complexity; i++){
-    password=password + values.charAt(Math.floor(Math.random() * Math.floor(values.length -1)));
-    }
-
-    //add password to text box
-    document.getElementById("password").value = password;
+function generatePassword ( characterAmount, includeUppercase, includeNumbers, includeSymbols) {
+  let charCodes = LOWERCASE_CHAR_CODES
+  if (includeUppercase) charCodes = charCodes.concat (UPPERCASE_CHAR_CODES) 
+  if (includeSymbols) charCodes = charCodes.concat (SYMBOL_CHAR_CODES)
+  if (includeNumbers) charCodes = charCodes.concat (NUMBER_CHAR_CODES)
+ 
+  const passwordCharacters = []
+  for (let i=0; i < characterAmount; i++) {
+    const characterCode = charCodes[Math.floor(Math.random() * charCodes.length)]
+    passwordCharacters.push(String.fromCharCode(characterCode))
+  }
+  return passwordCharacters.join('')
 }
 
-
+function arrayFromLowToHigh (low, high) {
+  const array = []
+  for (let i = low; i <= high; i++) {
+    array.push(i)
+  }
+  return array
 }
 
-
-
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+function syncCharacterAmount (e) {
+  const value = e.target.value
+  characterAmountNumber.value = value
+  characterAmountRange.value = value
+}
